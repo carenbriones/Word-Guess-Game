0fc2b3d96da9f1b4c885f3df4234983d4fffcb35
Caren B <carenpbriones@gmail.com>
2019-08-29 15:08:55 -0700

    Updated README.md, reordered game2.js with global functions at top, centered game on page

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Word-Guess-Game
 Game of hangman created using HTML, CSS, and Javascript
+
+### Gameplay
+A series of blanks is displayed on the screen, and each blank represents one letter in a randomly selected word from a word bank. The user has to guess the selected word, one letter at a time. If the user guesses a letter that is present in the word, the corresponding blank(s) will be filled in. If the user guesses a letter that isn't in the word, the user's number of guesses left will be decreased by 1. The game is over when the user either completes the word, which will increase their number of wins by 1, or they run out of guesses, will which decrease their number of losses by 1.
+
+* Note: Pressing the same key more than once only counts as one guess.
+
+* game.js was created without the use of objects, and game2.js was created with the game as an object. game2.js is the file linked to index.html
--- assets/javascript/game2.js
+++ assets/javascript/game2.js
@@ -1,57 +1,7 @@
-var MAX_GUESSES = 10;
+const MAX_GUESSES = 10;
 var characters = "abcdefghijklmnopqrstuvwxyz";
 
-var game = {
-    // Values that need to be stored in game:
-        // wins, losses, guessesLeft, lettersGuessed, correctGuesses
-        // wordBank, correctWord
-    wins: 0,
-    losses: 0,
-    guessesLeft: MAX_GUESSES,
-    lettersGuessed: [],
-    correctGuesses: "",
-    wordBank: ["turquoise", "chartreuse", "scarlet", "magenta", "ivory", "sepia", "amber", "mahogany", "fuchsia", "lavender", "mulberry", "cerulean", "sapphire", "ebony", "indigo", "obsidian",],
-    correctWord: "",
-
-    //Retrieved from https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
-    chooseRandomWord: function () {
-        var result = this.wordBank[Math.floor(Math.random() * this.wordBank.length)];
-        return result;
-    },
-
-    resetGame: function () {
-        this.guessesLeft = MAX_GUESSES;
-        this.lettersGuessed = [];
-        this.correctWord = this.chooseRandomWord();
-        this.correctGuesses = this.getWordBlanks(this.correctWord);
-        printStats();
-    },
-
-    getWordBlanks: function () {
-        var x = "";
-        for (i = 0; i < this.correctWord.length; i++) {
-            x += "_";
-        }
-        return x;
-    },
-
-    fillInBlanks: function (x) {
-        for (i = 0; i < this.correctWord.length; i++) {
-            if (x === this.correctWord[i]) { // If word contains letter, fill it in
-                this.correctGuesses = this.correctGuesses.substring(0, i) + x + this.correctGuesses.substring(i + 1)
-            }
-        }
-    },
-
-    formatCorrectGuesses: function () {
-        var x = "";
-        for (i = 0; i < this.correctGuesses.length; i++) {
-            x += this.correctGuesses[i] + " ";
-        }
-        return x;
-    }
-}
-
+// When the window first loads, display blanks for the correct word
 window.onload = function () {
     game.correctWord = game.chooseRandomWord();
     game.correctGuesses = game.getWordBlanks(game.correctWord);
@@ -61,17 +11,20 @@ window.onload = function () {
     wordText.innerHTML = game.formatCorrectGuesses(game.correctGuesses);
 }
 
-document.onkeyup = function(event){
+document.onkeyup = function (event) {
     var letterGuessed = event.key;
 
     // If key pressed is a letter
     if (characters.includes(letterGuessed)) {
+
         // If letter guessed is not in the answer
         if (!game.correctWord.includes(letterGuessed)) {
+
             // If letter has not been guessed already
             if (game.lettersGuessed.indexOf(letterGuessed) === -1) {
                 game.lettersGuessed.push(letterGuessed);
                 game.guessesLeft--;
+
                 // If there are no guesses left, user loses
                 if (game.guessesLeft === 0) {
                     game.resetGame();
@@ -84,28 +37,89 @@ document.onkeyup = function(event){
         }
         printStats(letterGuessed);
     }
-    if (game.correctGuesses === game.correctWord) { // If user completes the word, increment wins, reset game
+
+    // If user completes the word, increment wins, reset game
+    if (game.correctGuesses === game.correctWord) {
         game.wins++;
         game.resetGame();
     }
 }
 
+// Displays updated stats on the page
 function printStats(keyPressed) {
-
+    // Displays blanks for correct word
     var wordText = document.getElementById("word");
-
     wordText.innerHTML = game.formatCorrectGuesses(game.correctGuesses);
 
+    // Displays wins
     var winsText = document.getElementById("wins");
     winsText.innerHTML = "Wins: " + game.wins;
 
+    // Displays losses
     var lossesText = document.getElementById("losses");
     lossesText.innerHTML = "Losses: " + game.losses;
 
+    // Displays guesses left
     var guessesLeftText = document.getElementById("guesses-left");
     guessesLeftText.innerHTML = "Guesses left: " + game.guessesLeft;
 
+    // Displays guesses so far
     var guessesSoFarText = document.getElementById("guesses-so-far");
     guessesSoFarText.innerHTML = "Your guesses so far: " + game.lettersGuessed;
 
+}
+
+var game = {
+    // Values that need to be stored in game:
+    // wins, losses, guessesLeft, lettersGuessed, correctGuesses
+    // wordBank, correctWord
+    wins: 0,
+    losses: 0,
+    guessesLeft: MAX_GUESSES,
+    lettersGuessed: [],
+    correctGuesses: "",
+    wordBank: ["turquoise", "chartreuse", "scarlet", "magenta", "ivory", "sepia", "amber", "mahogany", "fuchsia", "lavender", "mulberry", "cerulean", "sapphire", "ebony", "indigo", "obsidian", "porcelain", "daffodil", "tangerine", "rouge", "mauve", "periwinkle", "amethyst", "eggplant", "cobalt", "pewter"],
+    correctWord: "",
+
+    //Retrieved from https://stackoverflow.com/questions/1349404/generate-random-string-characters-in-javascript
+    chooseRandomWord: function () {
+        var result = this.wordBank[Math.floor(Math.random() * this.wordBank.length)];
+        return result;
+    },
+
+    // Resets game stats to start a new game
+    resetGame: function () {
+        this.guessesLeft = MAX_GUESSES;
+        this.lettersGuessed = [];
+        this.correctWord = this.chooseRandomWord();
+        this.correctGuesses = this.getWordBlanks(this.correctWord);
+        printStats();
+    },
+
+    // Returns string of blanks the length of the correctWord
+    getWordBlanks: function () {
+        var x = "";
+        for (i = 0; i < this.correctWord.length; i++) {
+            x += "_";
+        }
+        return x;
+    },
+
+    // Fills in the corresponding blanks with the letter entered (x)
+    fillInBlanks: function (x) {
+        for (i = 0; i < this.correctWord.length; i++) {
+            if (x === this.correctWord[i]) { // If word contains letter, fill it in
+                this.correctGuesses = this.correctGuesses.substring(0, i) + x + this.correctGuesses.substring(i + 1)
+            }
+        }
+    },
+
+    // Formats string of blanks with spaces in between
+    formatCorrectGuesses: function () {
+        var x = "";
+        for (i = 0; i < this.correctGuesses.length; i++) {
+            x += this.correctGuesses[i] + " ";
+        }
+        return x;
+    }
 }
